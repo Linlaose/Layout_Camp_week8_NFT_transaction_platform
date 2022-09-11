ef7ab3e286b4bda2e5fc1d8f95f919bc506f24ab
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-12 03:10:34 +0800

    Update 2022-09-11T19:10:30.341Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,28 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+var headerLogo = document.querySelector('#header-logo');
+var iconBox = document.querySelector('#icon-box');
+var searchTextBar = document.querySelector('#search-text-bar');
+var goBack = document.querySelector('#go-back');
+var searchText = document.querySelector('#search-text');
+var search = document.querySelector('#search');
+var burger = document.querySelector('#burger');
+
+if (headerLogo) {
+  search.addEventListener('click', function () {
+    headerLogo.classList.add('hidden'); // 表頭 logo
+
+    iconBox.classList.add('justify-between', 'w-full'); // 表頭兩 icon 容器
+
+    searchTextBar.classList.remove('hidden'); // 顯現輸入容器
+
+    burger.classList.add('hidden'); // 漢堡選單
+  });
+  goBack.addEventListener('click', function () {
+    headerLogo.classList.remove('hidden');
+    iconBox.classList.remove('justify-between', 'w-full');
+    searchTextBar.classList.add('hidden');
+    burger.classList.remove('hidden');
+  });
+}
 //# sourceMappingURL=all.js.map
