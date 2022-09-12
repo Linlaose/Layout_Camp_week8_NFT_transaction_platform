6e82845fd2f64aa1eb40b301293090338c81c553
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-13 04:08:48 +0800

    更新(layout.ejs)：完成 PC 版表頭

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,21 +4,24 @@ const searchTextBar = document.querySelector('#search-text-bar');
 const goBack = document.querySelector('#go-back');
 const searchText = document.querySelector('#search-text');
 const search = document.querySelector('#search');
+const burgerBox = document.querySelector('#burger-box');
 const burger = document.querySelector('#burger');
 const burgerCancel = document.querySelector('#burger-cancel');
 const burgerContent = document.querySelector('#burger-content');
 
-if (headerLogo) {
+if (headerLogo && window.innerWidth < 640) {
   search.addEventListener('click', () => {
     headerLogo.classList.add('hidden'); // 表頭 logo
     iconBox.classList.add('justify-between', 'w-full'); // 表頭兩 icon 容器
     searchTextBar.classList.remove('hidden'); // 顯現輸入容器
+    burgerBox.classList.remove('w-10', 'pl-6');
     burger.classList.add('hidden'); // 漢堡選單
   });
   goBack.addEventListener('click', () => {
     headerLogo.classList.remove('hidden');
     iconBox.classList.remove('justify-between', 'w-full');
     searchTextBar.classList.add('hidden');
+    burgerBox.classList.add('w-10', 'pl-6');
     burger.classList.remove('hidden');
   });
   burger.addEventListener('click', () => {
