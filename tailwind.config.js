4b53fc64c3ede29cbacfacdacebfc4d9752e4a50
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-13 17:02:14 +0800

    更新(index.html)：Ranking 區塊排版完成，差 hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,14 +21,26 @@ module.exports = {
       width: {
         '9/10': '90%',
         '5/4': '125%',
+        '13.5': '3.375rem', /* 54px */
         '21.25': '5.3125rem', /* 85px */
         '47.5': '11.875rem',
       },
       height: {
         '4.5': '1.125rem', /* 18px */
+        '93.5': '23.375rem', /* 374px */
       },
       fontFamily: { // 空格記得要兩種引號
         'noto': ['"Noto Sans TC"',
+          '"-apple - system"',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"Microsoft JhengHei"',
+          '"Helvetica Neue"',
+          'monospace',
+          'Arial',
+          'cursive',
+          'sans-serif'],
+        'paytone': ['"Paytone One"',
           '"-apple - system"',
           'BlinkMacSystemFont',
           '"Segoe UI"',
