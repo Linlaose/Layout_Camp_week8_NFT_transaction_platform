217dafbc23d022b3091e2498bfa8ca863866d366
您的姓名 <98501671+Linlaose@users.noreply.github.com>
2022-09-13 09:41:52 +0800

    更新(layout.js)：補上遮罩

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,6 +8,7 @@ const burgerBox = document.querySelector('#burger-box');
 const burger = document.querySelector('#burger');
 const burgerCancel = document.querySelector('#burger-cancel');
 const burgerContent = document.querySelector('#burger-content');
+const mask = document.querySelector('#bg-mask');
 
 if (headerLogo && window.innerWidth < 640) {
   search.addEventListener('click', () => {
@@ -29,11 +30,13 @@ if (headerLogo && window.innerWidth < 640) {
     burger.classList.add('hidden');
     burgerCancel.classList.remove('hidden');
     burgerContent.classList.remove('invisible');
+    mask.classList.remove('opacity-0', '-z-10');
   });
   burgerCancel.addEventListener('click', () => {
     document.body.classList.remove('bg-gray-900/65');
     burger.classList.remove('hidden');
     burgerCancel.classList.add('hidden');
     burgerContent.classList.add('invisible');
+    mask.classList.add('opacity-0', '-z-10');
   })
 }
